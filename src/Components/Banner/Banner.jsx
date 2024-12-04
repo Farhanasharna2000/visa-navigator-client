982af41c8854fd6bc01dd7aae86cae119962d2b4
Farhanasharna2000 <farhanasharna22@gmail.com>
2024-12-04 15:04:12 +0600

    update banner section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,83 +5,33 @@ import slider4 from "../../assets/banner4.png";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full object-cover py-8">
-      <div
-        id="slide1"
-        className="carousel-item relative w-full bg-cover bg-center"
-        style={{ backgroundImage: `url(${slider1})` }}
-      >
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between z-10">
-          <a href="#slide4" className="btn btn-circle">❮</a>
-          <a href="#slide2" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img
-          src={slider2}
-          className="w-full h-[480px]"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide1" className="btn btn-circle">❮</a>
-          <a href="#slide3" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img
-          src={slider3}
-          className="w-full h-[480px]"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide2" className="btn btn-circle">❮</a>
-          <a href="#slide4" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img
-          src={slider4}
-          className="w-full h-[480px]"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide3" className="btn btn-circle">❮</a>
-          <a href="#slide1" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-
-      <style >{`
-
-        @keyframes slideAnimation {
-          0% {
-            transform: translateX(0%);
-          }
-          25% {
-            transform: translateX(-100%);
-          }
-          50% {
-            transform: translateX(-200%);
-          }
-          75% {
-            transform: translateX(-300%);
-          }
-          100% {
-            transform: translateX(0%);
-          }
-        }
-
-        .carousel {
-          display: flex;
-          overflow: hidden;
-          width: 100%;
-        }
-
-        .carousel-item {
-          flex: 0 0 100%;
-          transition: transform 1s ease-in-out;
-        }
-
-        .carousel > div {
-          animation: slideAnimation 25s infinite; 
-        }
-      `}</style>
+    <div className="my-8 bg-black">
+      <div className="carousel w-full h-[450px]">
+  <div id="item1" className="carousel-item w-full bg-cover bg-center"style={{ backgroundImage: `url(${slider1})` }}>
+  
+  </div>
+  <div id="item2" className="carousel-item w-full">
+    <img
+      src={slider2}
+      className="w-full" />
+  </div>
+  <div id="item3" className="carousel-item w-full">
+    <img
+      src={slider3}
+      className="w-full" />
+  </div>
+  <div id="item4" className="carousel-item w-full">
+    <img
+      src={slider4}
+      className="w-full" />
+  </div>
+</div>
+<div className="flex w-full justify-center gap-2 py-2">
+  <a href="#item1" className="btn btn-xs">1</a>
+  <a href="#item2" className="btn btn-xs">2</a>
+  <a href="#item3" className="btn btn-xs">3</a>
+  <a href="#item4" className="btn btn-xs">4</a>
+</div>
     </div>
   );
 };
